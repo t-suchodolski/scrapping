829f8fcc3811760e566162d3a1600b5e67d529c9
de-suo-faciendo-curavit <80784777+de-suo-faciendo-curavit@users.noreply.github.com>
2022-03-09 19:15:03 +0100

    Done and cleaned

--- main.py
+++ main.py
@@ -6,44 +6,6 @@ page = requests.get("https://www.autocentrum.pl/paliwa/ceny-paliw/")
 soup = BeautifulSoup(page.content, 'html.parser')
 price = soup.find(class_="table-responsive")
 items = price.find('td', class_='text-center').get_text()
-#print(price.prettify())
-
-
-def districts():
-    all_districts = []
-    list =soup.find_all('a', {"class": "row-link"})
-    for args in list:
-        all_districts.append(args.text.strip())
-    print(all_districts)
-
-def prices():
-    all_prices= []
-    chunk_size = 5
-    grouped_prices = list()
-    list_price = soup.find_all('a', {"class":"up"})
-    for prices in list_price:
-        all_prices.append(prices.text.strip())
-    for i in range(0, len(all_prices), chunk_size):
-        grouped_prices.append((all_prices[i:i+chunk_size]))
-    #print(grouped_prices)
-
-def fueltype():
-    fueltypes = []
-    for fuel in soup.find_all('div', {"class":"fuel-logo pb"}):
-        fueltypes.append(fuel.text.strip())
-    for fuel in soup.find_all('div', {"class":"fuel-logo pbp"}):
-        fueltypes.append(fuel.text.strip())
-    for fuel in soup.find_all('div', {"class":"fuel-logo on"}):
-        fueltypes.append(fuel.text.strip())
-    for fuel in soup.find_all('div', {"class":"fuel-logo onp"}):
-        fueltypes.append(fuel.text.strip())
-    for fuel in soup.find_all('div', {"class":"fuel-logo lpg"}):
-        fueltypes.append(fuel.text.strip())
-    print(fueltypes)
-
-#districts()
-#prices()
-#fueltype()
 
 #DISTRICTS
 all_districts = []
@@ -70,7 +32,6 @@ data = {'District': all_districts,
         'LPG': F_LPG
         }
 df = pd.DataFrame(data)
-#print(df)
 today = str(date.today())
 archive_name = str('Fuel Price ' + today + '.csv')
 print(archive_name)
